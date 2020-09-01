2af1f729a4261c2afb2d8dc43fef0b0e181eab0e
HatimAbdullah <HatimAlqiamh@gmail.com>
2020-09-01 13:47:20 +0300

    remove uneeded

--- cluster-prepare/pro-graf/pro-graf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm repo update
-helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=NodePort
